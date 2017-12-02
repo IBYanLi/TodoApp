07a69d4b6f9a191b86ff65a25fb445f0a91e490d
Yan <ibyanli@gmail.com>
2017-12-02 13:56:19 -0800

    mongo added

--- todoApp.js
+++ todoApp.js
@@ -1,7 +1,8 @@
-<!-- node todoApp.js to run -->
-console.log("Hello World!");
-console.log("sdfjdsfj");
-for (var i = 0; i < 10; i++) {
-  console.log(i);
-}
-"test"
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://todo:list@ds044667.mlab.com:44667/todoapp";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
